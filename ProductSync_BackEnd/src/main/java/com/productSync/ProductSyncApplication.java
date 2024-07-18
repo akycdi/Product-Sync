9ed9103574bb6a45aed89e444dc6645a389d7f15
Arun <arun36824@gmail.com>
2024-07-18 12:42:51 +0530

    jwt and exceptions

--- ProductSync_BackEnd/src/main/java/com/productSync/ProductSyncApplication.java
+++ ProductSync_BackEnd/src/main/java/com/productSync/ProductSyncApplication.java
@@ -1,13 +1,15 @@
 package com.productSync;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class ProductSyncApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductSyncApplication.class, args);
-	}
-
+		}
 }
